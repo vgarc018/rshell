041e4f2ab8a647d6a52d8fec6c330c4230314a98
Virgilio Garcia <virgiliogarcia@ucrwpa3-1-211-34.bulk.ucr.edu>
2015-02-03 15:24:46 -0800

    trying out -a flag

--- src/ls.cpp
+++ src/ls.cpp
@@ -7,24 +7,56 @@
 
 using namespace std;
 
-/*
- * This is a BARE BONES example of how to use opendir/readdir/closedir.
- * Notice that there is no error checking on these functions.
- * You MUST add error checking yourself.
- */
+
+void aFlag(const char *file)
+{
+    DIR *dirp; 
+    if((dirp = opendir(file)) == NULL) 
+    {
+        perror("Open Dir");
+        exit(1);
+    }
+    else
+    {
+        int direntp;
+        struct dirent *prevEntry = NULL;
+        struct dirent **dirEntry = NULL;
+        if((readdir_r(dirp,prevEntry,dirEntry)) != 0)
+        {
+            perror("reading dir");
+            exit(1);
+        }
+        else
+        {
+
+            while(!(direntp = readdir_r(dirp, prevEntry, dirEntry )))
+            {
+                cout << "It worked" << endl;
+            }
+            closedir(dirp);
+        }
+    }
+}
+
 
 int main()
 {
     string line;
     getline(cin,line);
 
-    cout << line << endl;
+    const char *filename = line.c_str();
+    aFlag(filename);
 
 
-    char *dirName = line;
+    return 0;
+    //cout << line << endl;
+
+
+   /* const char *dirName = line.c_str();
     DIR *dirp = opendir(dirName);
     dirent *direntp;
     while ((direntp = readdir(dirp)))
           cout << direntp->d_name << endl;  // use stat here to find attributes of file
     closedir(dirp);
+    */
 }
